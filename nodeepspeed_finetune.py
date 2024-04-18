58b40493c4eb4fbe8cffd34d414c074a072f4834
Tuan-Dung Bui <dungbuit1k28@gmail.com>
2024-04-18 20:32:59 +0700

    Update nodeepspeed_finetune.py

--- nodeepspeed_finetune.py
+++ nodeepspeed_finetune.py
@@ -144,7 +144,7 @@ def deepseek_train_tokenize_function(examples, tokenizer, task):
     elif 'final' in task:
         sources = [
             deepseek_build_masked_func(instruction) + '\n<ouput>\n' + output + '\n<compile>\n' + deepseek_build_output_compiler(compile_info) + '\n<inherit>\n' + inherit_elements + '\n<correct> '
-            for (instruction, output, compile_info, inherit_elements) in zip(examples['masked_class'], examples['deepseek_output'], examples['compile_info'], examples['inherit_elements'])
+            for (instruction, output, compile_info, inherit_elements) in zip(examples['masked_class'], examples['finetune_output'], examples['compile_info'], examples['inherit_elements'])
         ]
         targets = [f"{output}\n{EOT_TOKEN}" for output in examples['func_body']]
         data_dict = preprocess(sources, targets, tokenizer)
