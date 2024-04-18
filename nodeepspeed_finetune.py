20da084f49d18dad93353165be38f6180f9e89c5
Tuan-Dung Bui <dungbuit1k28@gmail.com>
2024-04-18 20:07:18 +0700

    Update nodeepspeed_finetune.py

--- nodeepspeed_finetune.py
+++ nodeepspeed_finetune.py
@@ -305,7 +305,6 @@ def train(args):
             starcoder_train_tokenize_function,
             batched=True,
             batch_size=3000,
-            num_proc=32,
             remove_columns=raw_train_datasets.column_names,
             load_from_cache_file=True, # not args.overwrite_cache
             desc="Running Encoding",
