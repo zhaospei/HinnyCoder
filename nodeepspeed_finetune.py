bf5a440ad75e4022f473d5b28c647ae6fa9be669
Tuan-Dung Bui <dungbuit1k28@gmail.com>
2024-04-30 13:26:37 +0700

    Update nodeepspeed_finetune.py

--- nodeepspeed_finetune.py
+++ nodeepspeed_finetune.py
@@ -186,7 +186,7 @@ def gemma_train_tokenize_function(examples, tokenizer, task):
     elif 'final' in task:
         sources = [
             gemma_build_masked_func(instruction) + '\n<ouput>\n' + output + '\n<compile>\n' + deepseek_build_output_compiler(compile_info) + '\n<inherit>\n' + inherit_elements + '\n<correct> '
-            for (instruction, output, compile_info, inherit_elements) in zip(examples['masked_class'], examples['finetune_output'], examples['compile_info'], examples['inherit_elements'])
+            for (instruction, output, compile_info, inherit_elements) in zip(examples['masked_contract'], examples['finetune_output'], examples['compile_info'], examples['inherit_elements'])
         ]
         targets = [f"{output}" + tokenizer.eos_token  for output in examples['func_body']]
         data_dict = preprocess(sources, targets, tokenizer)
