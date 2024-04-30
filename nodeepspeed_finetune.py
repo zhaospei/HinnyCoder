e7ea2e54586f0149aeb6b0e568a42271b90eac7b
Tuan-Dung Bui <dungbuit1k28@gmail.com>
2024-04-30 13:29:28 +0700

    remove num_proc=32

--- nodeepspeed_finetune.py
+++ nodeepspeed_finetune.py
@@ -267,7 +267,7 @@ def train(args):
             deepseek_train_tokenize_function,
             batched=True,
             batch_size=3000,
-            num_proc=32,
+            
             remove_columns=raw_train_datasets.column_names,
             load_from_cache_file=True, # not args.overwrite_cache
             desc="Running Encoding",
@@ -278,7 +278,7 @@ def train(args):
             codellama_train_tokenize_function,
             batched=True,
             batch_size=3000,
-            num_proc=32,
+            
             remove_columns=raw_train_datasets.column_names,
             load_from_cache_file=True, # not args.overwrite_cache
             desc="Running Encoding",
@@ -289,7 +289,7 @@ def train(args):
             gemma_train_tokenize_function,
             batched=True,
             batch_size=3000,
-            num_proc=32,
+            
             remove_columns=raw_train_datasets.column_names,
             load_from_cache_file=True, # not args.overwrite_cache
             desc="Running Encoding",
@@ -300,7 +300,7 @@ def train(args):
             starcoder_train_tokenize_function,
             batched=True,
             batch_size=3000,
-            num_proc=32,
+            
             remove_columns=raw_train_datasets.column_names,
             load_from_cache_file=True, # not args.overwrite_cache
             desc="Running Encoding",
