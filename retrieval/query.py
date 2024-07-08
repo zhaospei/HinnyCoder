356bb7778f7beca9611692843758dc00d25f5481
aqu4holic <gautruc.anh1234@gmail.com>
2024-07-08 21:35:09 +0700

    add flags to query fn

--- retrieval/query.py
+++ retrieval/query.py
@@ -4,7 +4,26 @@ from tqdm import tqdm
 import pandas as pd
 import argparse
 import json
-import time
+import os
+
+uri = 'http://localhost:19530'
+data_prefix = 'data'
+raw_prefix = f'{data_prefix}/raw'
+db_prefix = f'{data_prefix}/database'
+
+data_type = ['types']
+search_params = {
+    'metric_type': 'L2',
+    'params': {
+        'nprobe': 1024,
+    },
+}
+
+embedding_model = model.hybrid.BGEM3EmbeddingFunction(
+    model_name = 'BAAI/bge-m3',
+    device = 'cuda:0',
+    use_fp16 = False,
+)
 
 def scan_repo_list():
     import os
@@ -13,7 +32,7 @@ def scan_repo_list():
     modified_filenames = set()
 
     # Directory containing the files
-    directory = 'data/raw'
+    directory = raw_prefix
 
     # Loop through each file in the specified directory
     for filename in os.listdir(directory):
@@ -61,33 +80,17 @@ def create_repo_list(scan_db = False):
 
     return encoded_repo_list, repo_list_map
 
-uri = 'http://localhost:19530'
-data_prefix = 'data'
-raw_prefix = f'{data_prefix}/raw'
-db_prefix = f'{data_prefix}/database'
-
-data_type = ['types']
-search_params = {
-    'metric_type': 'L2',
-    'params': {
-        'nprobe': 1024,
-    },
-}
-
-embedding_model = model.hybrid.BGEM3EmbeddingFunction(
-    model_name = 'BAAI/bge-m3',
-    device = 'cuda:0',
-    use_fp16 = False,
-)
-
-def retrieve(client, db_name, embedded_retrieval_elements):
+def retrieve(client, db_name, proj_name, relative_path, embedded_retrieval_elements):
     client = MilvusClient(
         uri = uri,
         db_name = db_name,
     )
 
     relevant_context = ''
+    relevant_context_no_cmt = ''
+    current_relative_path = os.path.join(proj_name, relative_path)
     all_names = []
+    retrieved_name_set = set()
     for dt in data_type:
         if (dt not in embedded_retrieval_elements):
             continue
@@ -121,17 +124,21 @@ def retrieve(client, db_name, embedded_retrieval_elements):
             return
 
         for i in range(len(retrieved_candidates)):
-            candidate = retrieved_candidates[i]
-            ids = [int(candidate[0]['id'])]
+            candidates = retrieved_candidates[i]
+            ids = [int(candidates[j]['id']) for j in range(len(candidates))]
 
-            res = client.get(
-                collection_name = dt,
-                ids = ids,
-            )
+            wres = []
+            for ai_di in ids:
+                res = client.get(
+                    collection_name = dt,
+                    ids = [ai_di],
+                )
+
+                wres.append(res[0])
 
             all_res.append(
                 {
-                    names[i]: res[0],
+                    names[i]: wres,
                 }
             )
 
@@ -139,27 +146,51 @@ def retrieve(client, db_name, embedded_retrieval_elements):
             collection_name = dt,
         )
 
-        f = open(all_res[0][names[0]]['data_path'], 'r')
+        f = open(all_res[0][names[0]][0]['data_path'], 'r')
         datas = json.load(f)
         f.close()
 
         for i in range(len(all_res)):
             name = names[i]
-            data = all_res[i][name]
+            bundled_data = all_res[i][name]
+            data = None
 
-            retrieved_name = datas[data['id']]['name']
+            retrieved_name = ''
+            for r in bundled_data:
+                r_relative_path = datas[r['id']]['relative_path']
+
+                if (r_relative_path == current_relative_path):
+                    continue
+                else:
+                    data = r
+                    retrieved_name = datas[data['id']]['name']
+
+                    break
+
             all_names.append(
                 {
                     name: retrieved_name,
                 }
             )
 
+            if (data is None):
+                continue
+
             if (dt == 'types'):
+                if (retrieved_name in retrieved_name_set):
+                    continue
+                else:
+                    retrieved_name_set.add(retrieved_name)
+
+                # print(proj_name, datas[data['id']].keys())
+
                 relevant_context += datas[data['id']]['abstract']
+                if ('abstract_compact' in datas[data['id']].keys()):
+                    relevant_context_no_cmt += datas[data['id']]['abstract_compact']
             else:
                 relevant_context += datas[data['id']]['raw']
 
-    return relevant_context, all_names
+    return relevant_context, relevant_context_no_cmt, all_names
 
 class query_by_thread(Thread):
     def __init__(self, client, db_name, embedded_retrieval_elements, testcase, df):
@@ -171,30 +202,40 @@ class query_by_thread(Thread):
         self.df = df
 
     def run(self):
-        res, all_names = retrieve(self.client, self.db_name, self.embedded_retrieval_elements)
+        proj_name = self.df.iloc[self.testcase]['proj_name']
+        relative_path = self.df.iloc[self.testcase]['relative_path']
+        res, res_no_cmt, all_names = retrieve(
+            self.client, self.db_name, proj_name, relative_path, self.embedded_retrieval_elements
+        )
 
         self.df.at[self.testcase, 'relevant_context'] = res
+        self.df.at[self.testcase, 'relevant_context_no_cmt'] = res_no_cmt
         self.df.at[self.testcase, 'retrieved_names'] = json.dumps(all_names)
 
 class create_work_queue_by_thread(Thread):
-    def __init__(self, id, row, work_queue, repo_list_map):
+    def __init__(self, id, row, work_queue, repo_list_map, retrieval_column):
         Thread.__init__(self)
         self.id = id
         self.row = row
         self.work_queue = work_queue
         self.repo_list_map = repo_list_map
+        self.retrieval_column = retrieval_column
 
     def run(self):
         row = self.row
         work_queue = self.work_queue
         repo_list_map = self.repo_list_map
         id = self.id
+        retrieval_column = self.retrieval_column
 
         db_name = repo_list_map[row['proj_name']]
-        retrieval_elements = row['retrieval_element']
+        retrieval_elements = row[retrieval_column]
         embedded_retrieval_elements = {}
 
-        for element in retrieval_elements:
+        for element in data_type:
+            if (element not in retrieval_elements.keys()):
+                continue
+
             if (len(retrieval_elements[element]) == 0):
                 continue
 
@@ -255,7 +296,7 @@ def main():
         '--src_name',
         type = str,
         help = 'data source file name',
-        default = 'i_did_it.parquet',
+        default = 'source.parquet',
     )
 
     parser.add_argument(
@@ -263,6 +304,13 @@ def main():
         type = str,
         help = 'saved file name',
         default = 'i_did_it.parquet',
+    )
+
+    parser.add_argument(
+        '--retrieval_column',
+        type = str,
+        help = 'column contains the retrieval elements',
+        default = 'retrieval_element',
     )
 
     args = parser.parse_args()
@@ -278,11 +326,14 @@ def main():
     df = pd.read_parquet(f'{data_prefix}/{data_name}')
     if ('relevant_context' not in df.columns):
             df['relevant_context'] = None
+    if ('relevant_context_no_cmt' not in df.columns):
+            df['relevant_context_no_cmt'] = None
     if ('retrieved_names' not in df.columns):
         df['retrieved_names'] = None
         df['retrieved_names'] = df['retrieved_names'].astype(object)
     if (args.reset):
         df['relevant_context'] = None
+        df['relevant_context_no_cmt'] = None
         df['retrieved_names'] = None
         df['retrieved_names'] = df['retrieved_names'].astype(object)
     res_name = args.name
@@ -318,6 +369,7 @@ def main():
             row = row,
             work_queue = work_queue,
             repo_list_map = repo_list_map,
+            retrieval_column = args.retrieval_column
         )
         b.start()
         threads.append(b)
