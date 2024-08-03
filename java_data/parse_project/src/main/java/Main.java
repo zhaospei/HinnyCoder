243303f13305f5f0e46a8139f04890e1ce6a24b1
echodrift <luuvanducthieu291@gmail.com>
2024-08-03 18:23:39 +0700

    Update parse project

--- java_data/parse_project/src/main/java/Main.java
+++ java_data/parse_project/src/main/java/Main.java
@@ -4,8 +4,8 @@ import flute.utils.file_processing.FileProcessor;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
-// import org.eclipse.jface.text.Document;
-// import org.eclipse.text.edits.TextEdit;
+import org.eclipse.jface.text.Document;
+import org.eclipse.text.edits.TextEdit;
 
 import com.google.gson.*;
 
@@ -24,7 +24,7 @@ import java.util.stream.Stream;
 import java.util.List;
 import java.util.ArrayList;
 
-import me.tongfei.progressbar.ProgressBar;
+// import me.tongfei.progressbar.ProgressBar;
 
 public class Main {
     private static class Visitor extends ASTVisitor {
@@ -40,6 +40,7 @@ public class Main {
             this.source = source;
         }
 
+        // 3 types below are children of AbstractTypeDeclaration
         @Override
         public boolean visit(TypeDeclaration node) {
             String name = node.getName().getIdentifier();
@@ -78,6 +79,7 @@ public class Main {
             classes.add(currentClass);
             return super.visit(node);
         }
+
         // @Override
         // public boolean visit(FieldDeclaration node) {
         // // Get the type of the field
@@ -109,7 +111,7 @@ public class Main {
         public boolean visit(MethodDeclaration node) {
             JsonObject method = new JsonObject();
             method.addProperty("relative_path", relativePath);
-            String name = node.getName().toString();
+            String name = node.getName().getIdentifier();
             method.addProperty("name", name);
             String returnType = node.getReturnType2() != null ? node.getReturnType2().toString() : "void";
             method.addProperty("return_type", returnType);
@@ -123,7 +125,7 @@ public class Main {
             for (Object param : node.parameters()) {
                 SingleVariableDeclaration paramDecl = (SingleVariableDeclaration) param;
                 String paramType = paramDecl.getType().toString();
-                String paramName = paramDecl.getName().toString();
+                String paramName = paramDecl.getName().getIdentifier();
                 JsonObject paramInfo = new JsonObject();
                 paramInfo.addProperty("type", paramType);
                 paramInfo.addProperty("name", paramName);
@@ -173,16 +175,6 @@ public class Main {
         }
     }
 
-    public static CompilationUnit parse(String source) {
-        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
-        parser.setSource(source.toCharArray());
-        // parser.setResolveBindings(true);
-        parser.setKind(ASTParser.K_COMPILATION_UNIT);
-        // parser.setBindingsRecovery(true);
-        // parser.setStatementsRecovery(true);
-        return (CompilationUnit) parser.createAST(null);
-    }
-
     public static class MethodSignatureVisitor extends ASTVisitor {
         private final ASTRewrite rewriter;
 
@@ -217,7 +209,8 @@ public class Main {
             rewriter.remove(node, null);
             return false;
         }
-
+        // 3 types below are children class of Annotation 
+        // (3 kinds of annotation according to jdt definition)
         @Override
         public boolean visit(MarkerAnnotation node) {
             rewriter.remove(node, null);
@@ -236,6 +229,21 @@ public class Main {
             return false;
         }
 
+    }
+
+    public static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public static CompilationUnit parse(String source) {
+            ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+            parser.setSource(source.toCharArray());
+            // parser.setResolveBindings(true);
+            parser.setKind(ASTParser.K_COMPILATION_UNIT);
+            // parser.setBindingsRecovery(true);
+            // parser.setStatementsRecovery(true);
+            return (CompilationUnit) parser.createAST(null);
     }
 
     private static CompilationUnit createCU(String projectName, String projectDir, String file) {
@@ -260,96 +268,93 @@ public class Main {
         return cu;
     }
 
-    // private static JsonArray parseType(CompilationUnit cu, String relativePath,
-    // Logger logger) {
-    // int numType = cu.types().size();
-    // if (numType == 0) {
-    // logger.info("The file has no class: " + relativePath);
-    // return null;
-    // } else {
-    // JsonArray result = new JsonArray();
-    // for (int i = 0; i < numType; i++) {
-    // JsonObject typeInfo = new JsonObject();
-    // typeInfo.addProperty("relative_path", relativePath);
-    // AbstractTypeDeclaration type = (AbstractTypeDeclaration) cu.types().get(i);
-    // typeInfo.addProperty("name", type.getName().toString());
-    // String modifiers = "";
-    // for (Object modifier : type.modifiers()) {
-    // modifiers += modifier.toString() + " ";
-    // }
-    // modifiers = modifiers.replaceAll("\\s+$", "");
-    // typeInfo.addProperty("modifiers", modifiers);
-    // ITypeBinding binding = type.resolveBinding();
-    // if (binding == null) {
-    // typeInfo.addProperty("qualified_name", "<cant_resolve_binding>");
-    // } else {
-    // typeInfo.addProperty("qualified_name", binding.getQualifiedName());
-    // }
+    private static JsonArray parseType(CompilationUnit cu, String relativePath) {
+        int numType = cu.types().size();
+        if (numType == 0) {
+            // logger.info("The file has no class: " + relativePath);
+            return null;
+        } else {
+            JsonArray result = new JsonArray();
+            for (int i = 0; i < numType; i++) {
+                JsonObject typeInfo = new JsonObject();
+                typeInfo.addProperty("relative_path", relativePath);
+                AbstractTypeDeclaration type = (AbstractTypeDeclaration) cu.types().get(i);
+                typeInfo.addProperty("name", type.getName().getIdentifier());
+                String modifiers = "";
+                for (Object modifier : type.modifiers()) {
+                    modifiers += modifier.toString() + " ";
+                }
+                modifiers = modifiers.replaceAll("\\s+$", "");
+                typeInfo.addProperty("modifiers", modifiers);
+                ITypeBinding binding = type.resolveBinding();
+                if (binding == null) {
+                    typeInfo.addProperty("qualified_name", "<cant_resolve_binding>");
+                } else {
+                    typeInfo.addProperty("qualified_name", binding.getQualifiedName());
+                }
 
-    // // Get the superclass name
-    // try {
-    // TypeDeclaration classType = (TypeDeclaration) type;
-    // Type superClassType = classType.getSuperclassType();
-    // if (superClassType != null) {
-    // typeInfo.addProperty("extend", superClassType.toString());
-    // } else {
-    // typeInfo.addProperty("extend", "");
-    // }
-    // String interfaces = "";
-    // for (Object interfaceType : classType.superInterfaceTypes()) {
-    // interfaces += interfaceType.toString() + ' ';
-    // }
-    // interfaces = interfaces.replaceAll("\\s+$", "");
-    // typeInfo.addProperty("implements", interfaces);
-    // } catch (Exception e) {
-    // typeInfo.addProperty("extend", "");
-    // typeInfo.addProperty("implements", "");
-    // }
-    // typeInfo.addProperty("raw", type.toString());
-
-    // // Get raw class without method body
-    // try {
-    // String source = type.toString();
-    // CompilationUnit classCU = parse(source);
-    // AST ast = classCU.getAST();
-    // ASTRewrite rewriter = ASTRewrite.create(ast);
-    // MethodSignatureVisitor visitor = new MethodSignatureVisitor(rewriter);
-    // classCU.accept(visitor);
-    // Document document = new Document(source);
-    // TextEdit edits = rewriter.rewriteAST(document, null);
-    // edits.apply(document);
-    // typeInfo.addProperty("abstract", document.get());
-    // // System.out.println("Abstract:");
-    // // System.out.println(document.get());
-    // // System.out.println("-----------------------------------");
-    // } catch (Exception e) {
-    // typeInfo.addProperty("abstract", "error");
-    // // System.out.println("Abstract: Error");
-    // }
-    // // Get raw class without method body, javadoc, annotation
-    // try {
-    // String source = type.toString();
-    // CompilationUnit classCU = parse(source);
-    // ASTRewrite rewriter2 = ASTRewrite.create(classCU.getAST());
-
-    // MethodSignatureVisitor2 visitor2 = new MethodSignatureVisitor2(rewriter2);
-    // classCU.accept(visitor2);
-    // Document document2 = new Document(source);
-    // TextEdit edits2 = rewriter2.rewriteAST(document2, null);
-    // edits2.apply(document2);
-    // typeInfo.addProperty("abstract_compact", document2.get());
-    // // System.out.println("Abstract compact:");
-    // // System.out.println(document2.get());
-    // // System.out.println("-----------------------------------");
-    // } catch (Exception e) {
-    // typeInfo.addProperty("abstract_compact", "error");
-    // // System.out.println("Error");
-    // }
-    // result.add(typeInfo);
-    // }
-    // return result;
-    // }
-    // }
+                // Get the superclass name
+                try {
+                    TypeDeclaration classType = (TypeDeclaration) type;
+                    Type superClassType = classType.getSuperclassType();
+                    if (superClassType != null) {
+                        typeInfo.addProperty("extend", superClassType.toString());
+                    } else {
+                        typeInfo.addProperty("extend", "");
+                    }
+                    String interfaces = "";
+                    for (Object interfaceType : classType.superInterfaceTypes()) {
+                        interfaces += interfaceType.toString() + ' ';
+                    }
+                    interfaces = interfaces.replaceAll("\\s+$", "");
+                    typeInfo.addProperty("implements", interfaces);
+                } catch (Exception e) {
+                    typeInfo.addProperty("extend", "");
+                    typeInfo.addProperty("implements", "");
+                }
+                typeInfo.addProperty("raw", type.toString());
+                // Get raw class without method body
+                try {
+                    String source = type.toString();
+                    CompilationUnit classCU = parse(source);
+                    AST ast = classCU.getAST();
+                    ASTRewrite rewriter = ASTRewrite.create(ast);
+                    MethodSignatureVisitor visitor = new MethodSignatureVisitor(rewriter);
+                    classCU.accept(visitor);
+                    Document document = new Document(source);
+                    TextEdit edits = rewriter.rewriteAST(document, null);
+                    edits.apply(document);
+                    typeInfo.addProperty("abstract", document.get());
+                    // System.out.println("Abstract:");
+                    // System.out.println(document.get());
+                    // System.out.println("-----------------------------------");
+                } catch (Exception e) {
+                    typeInfo.addProperty("abstract", "error");
+                    // System.out.println("Abstract: Error");
+                }
+                // Get raw class without method body, javadoc, annotation
+                try {
+                    String source = type.toString();
+                    CompilationUnit classCU = parse(source);
+                    ASTRewrite rewriter2 = ASTRewrite.create(classCU.getAST());
+                    MethodSignatureVisitor2 visitor2 = new MethodSignatureVisitor2(rewriter2);
+                    classCU.accept(visitor2);
+                    Document document2 = new Document(source);
+                    TextEdit edits2 = rewriter2.rewriteAST(document2, null);
+                    edits2.apply(document2);
+                    typeInfo.addProperty("abstract_compact", document2.get());
+                    // System.out.println("Abstract compact:");
+                    // System.out.println(document2.get());
+                    // System.out.println("-----------------------------------");
+                } catch (Exception e) {
+                    typeInfo.addProperty("abstract_compact", "error");
+                    // System.out.println("Error");
+                }
+                result.add(typeInfo);
+            }
+            return result;
+        }
+    }
 
     // private static JsonArray parseField(CompilationUnit cu, String relativePath,
     // Logger logger) {
@@ -358,11 +363,6 @@ public class Main {
     // return fieldVisitor.getFields();
     // }
 
-    public static String readFile(String path, Charset encoding) throws IOException {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded, encoding);
-    }
-
     private static JsonArray parseMethod(CompilationUnit cu, String relativePath, String source) {
         Visitor methodVisitor = new Visitor(relativePath, source);
         cu.accept(methodVisitor);
@@ -370,8 +370,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-        Config.JAVAFX_DIR = "/home/hieuvd/lvdthieu/javafx-jmods-17.0.10";
+        Config.JAVAFX_DIR = "/home/lvdthieu/";
         String baseDir = args[0];
         String projectName = args[1];
         String projectDir = baseDir + "/" + projectName;
@@ -383,29 +382,27 @@ public class Main {
                     .collect(Collectors.toList()));
         } catch (Exception e) {
         }
-        // JsonArray types = new JsonArray();
+        JsonArray types = new JsonArray();
         JsonArray methods = new JsonArray();
         // JsonArray fields = new JsonArray();
         // List tmp = new ArrayList();
         // tmp.add("/home/lvdthieu/code-gen/javamelody_javamelody/javamelody/javamelody-core/src/main/java/net/bull/javamelody/PayloadNameRequestWrapper.java");
         // files = tmp;
         if (files.size() > 0) {
-
-            for (String file : ProgressBar.wrap(files, "Parsing")) {
+            for (String file : files) {
                 try {
                     CompilationUnit cu = createCU(projectName, projectDir, file);
                     String relativePath = file.replace(baseDir + '/', "");
                     String source = readFile(file, Charset.forName("UTF-8")).replaceAll("\r\n", "\n");
-                    // JsonArray typeInfos = parseType(cu, relativePath, logger);
-                    // if (typeInfos != null) {
-                    // types.addAll(typeInfos);
-                    // }
+                    JsonArray typeInfos = parseType(cu, relativePath);
+                    if (typeInfos != null) {
+                        types.addAll(typeInfos);
+                    }
                     // JsonArray fieldInfos = parseField(cu, relativePath, logger);
                     // if (fieldInfos != null) {
                     // fields.addAll(fieldInfos);
                     // }
                     JsonArray methodInfos = parseMethod(cu, relativePath, source);
-
                     if (methodInfos != null) {
                         methods.addAll(methodInfos);
                     }
@@ -415,13 +412,12 @@ public class Main {
 
             }
         }
-        // try (Writer writer = new OutputStreamWriter(new FileOutputStream(projectName
-        // + "_types.json"), "UTF-8")) {
-        // Gson gson = new GsonBuilder().create();
-        // gson.toJson(types, writer);
-        // } catch (Exception e) {
-        // logger.severe(e.getMessage());
-        // }
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(projectName
+                + "_types.json"), "UTF-8")) {
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(types, writer);
+        } catch (Exception e) {
+        }
         // try (Writer writer = new OutputStreamWriter(new FileOutputStream(projectName
         // + "_fields.json"), "UTF-8")) {
         // Gson gson = new GsonBuilder().create();
@@ -435,8 +431,6 @@ public class Main {
             Gson gson = new GsonBuilder().create();
             gson.toJson(methods, writer);
         } catch (Exception e) {
-
         }
-
     }
 }
